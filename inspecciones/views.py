67e7ce6d47ebe22d4579fa7f1e5c332c530963a1
Gregory Hernandez <ha.alexis20@gmail.com>
2024-05-03 15:29:12 -0400

    Agregado Mapa Semanal

--- inspecciones/views.py
+++ inspecciones/views.py
@@ -5,6 +5,9 @@ from django.http import HttpRequest
 from django.contrib import messages
 from django import forms
 
+import calendar
+from datetime import datetime, timedelta
+
 from django.utils import timezone
 
 from django.contrib.auth.decorators import login_required
@@ -496,3 +499,68 @@ def fuentedevulnerabilidad_delete(request,fuentedevulnerabilidad_id):
 def componente_list(request):
     componentes = Componente.objects.all()
     return render(request, 'componente/list.html', {'componentes': componentes})
+
+
+
+def reporte_probabilidades_semana(request):
+    # Obtener el año actual
+    años_disponibles = Inspeccion.objects.values_list('fecha__year', flat=True).distinct()
+    semanas = range(1, 53)
+    año_actual = request.GET.get('año', None)
+    año_actual = int(año_actual) if año_actual else None
+
+    # Si no se proporciona un claño, usar el año actual
+    if año_actual is None:
+        año_actual = timezone.now().year
+
+    # Obtener todos los componentes
+    componentes = Componente.objects.all()
+
+    # Crear un diccionario para almacenar los datos del informe
+    reporte = {}
+
+    # Obtener el número total de semanas en el año
+    num_semanas = 52
+
+    # Iterar sobre cada componente
+    for componente in componentes:
+        # Crear una entrada en el diccionario de informes para este componente
+        reporte[componente] = {}
+
+        # Iterar sobre cada semana del año
+        for semana in range(1, num_semanas + 1):
+            # Obtener la fecha de inicio y fin de la semana
+            fecha_inicio_semana = timezone.make_aware(timezone.datetime(año_actual, 1, 1)) + timedelta(weeks=semana - 1)
+            fecha_fin_semana = fecha_inicio_semana + timedelta(days=6)
+
+            # Filtrar inspecciones para este componente y esta semana
+            inspecciones = Inspeccion.objects.filter(
+                componente=componente,
+                fecha__range=(fecha_inicio_semana, fecha_fin_semana)
+            ).order_by('-fecha')
+
+            # Obtener la última inspección de esta semana
+            ultima_inspeccion = inspecciones.first()
+
+            # Si no hay inspecciones en esta semana, usar la última inspección anterior
+            if not ultima_inspeccion:
+                inspeccion_anterior = Inspeccion.objects.filter(
+                    componente=componente,
+                    fecha__lt=fecha_inicio_semana
+                ).order_by('-fecha').first()
+                if inspeccion_anterior:
+                    vulnerabilidad = inspeccion_anterior.vulnerabilidad.valor
+                    color = inspeccion_anterior.vulnerabilidad.color
+                else:
+                    vulnerabilidad = None
+                    color = '#000000'
+            else:
+                vulnerabilidad = ultima_inspeccion.vulnerabilidad.valor
+                color = ultima_inspeccion.vulnerabilidad.color
+
+
+            # Almacenar la vulnerabilidad en el diccionario de informes
+            reporte[componente][semana] = {'vulnerabilidad': vulnerabilidad, 'color': color}
+
+    # Renderizar el template con los datos del informe
+    return render(request, 'reporte_probabilidades_semanal.html', {'reporte': reporte, 'año_actual': año_actual,'años_disponibles':años_disponibles,'semanas':semanas})
