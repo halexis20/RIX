0d9461266deb4bc4ba694e4b46fb27b1540870ee
Gregory Hernandez <ha.alexis20@gmail.com>
2024-05-15 13:03:21 -0400

    ajustes

--- PRX/settings.py
+++ PRX/settings.py
@@ -12,18 +12,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path,os
 
-from decouple import config
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-# Lee las credenciales de email desde el archivo .env
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
-DEFAULT_FROM_EMAIL =EMAIL_HOST_USER
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
