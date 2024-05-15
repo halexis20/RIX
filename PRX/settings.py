406c8ff4e481fc6f910ba790a8e7681582bdec6c
Gregory Hernandez <ha.alexis20@gmail.com>
2024-05-15 13:00:43 -0400

    Ajustes de set

--- PRX/settings.py
+++ PRX/settings.py
@@ -12,6 +12,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path,os
 
+from decouple import config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# Lee las credenciales de email desde el archivo .env
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL =EMAIL_HOST_USER
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,7 +154,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'inspecciones/static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'inspecciones/statico'),
+    os.path.join(BASE_DIR, 'PRX/static'),
 ]
 
 MEDIA_URL = '/fotos/'
